e6fbce4eff112c68ddd960e4c2629b3192bb9433
unknown <ryanmoralesaz@gmail.com>
2026-08-14 21:28:17 -0700

    Add Print2Console script and vscode config, update sample scene

---
+++ Assets/Print2Console.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Print2Console : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       Debug.Log("I am writing code!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
